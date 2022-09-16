d364cd78d15ab0a49b61635769a55e7d382452de
Lynn <griff.lynn8@gmail.com>
2022-09-16 16:14:14 -0700

    database GET queries have been set up. post queries to come when I find the right syntax

---
+++ server/db.js
@@ -0,0 +1,38 @@
+const { Pool, Client } = require('pg')
+const Pass = require('../env/config.js');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'atelier',
+  password: Pass.PASS,
+  port: 5432,
+});
+pool.connect();
+
+module.exports = {
+
+  getQs: pool.query("SELECT json_agg(q) FROM q", (err, res) => {
+    console.log(err, res);
+  }),
+
+  getAs: pool.query("SELECT json_agg(a) FROM a", (err, res) => {
+    console.log(err, res);
+  }),
+
+  getPhotos: pool.query("SELECT json_agg(a_photos) FROM a_photos", (err, res) => {
+    console.log(err, res);
+  }),
+
+  postQ: pool.query("", (err, res) => {
+    console.log(err, res);
+  }),
+
+  postA: pool.query("", (err, res) => {
+    console.log(err, res);
+  }),
+
+  postPhoto: pool.query("", (err, res) => {
+    console.log(err, res);
+  }),
+}
