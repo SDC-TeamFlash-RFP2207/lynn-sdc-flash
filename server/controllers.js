5ec46edc923aec718ae5d827612c48320f0c77d6
Lynn <griff.lynn8@gmail.com>
2022-09-20 09:50:07 -0700

    working on queries

--- server/controllers.js
+++ server/controllers.js
@@ -2,16 +2,53 @@ const models = require('./db-mod.js');
 
 module.exports = {
   getAll: (req, res) => {
-    console.log('path conroller getAll works!');
-    console.log(models.getQs(req));
+    console.log('path controller getAll works!');
     models.getQs(req.query).then((data) => {
       res.status(200).json(data.rows);
     });
   },
 
   getAnswers: (req, res) => {
-    models.getAs(req.query).then((data) => {
+    console.log('path controller getAnswers works!');
+    console.log('req.query:', req.params);
+    models.getAs(req.params).then((data) => {
       res.status(200).json(data.rows);
+    });
+  },
+
+  postQuestion: (req, res) => {
+    models.postQ(req.query).then((data) => {
+      res.status(201);
+    })
+  },
+
+  postAnswer: (req, res) => {
+    models.postA(req.query).then((data) => {
+      res.status(201);
+    })
+  },
+
+  putQHelpful: (req, res) => {
+    models.putQHelp(req.query).then((data) => {
+      res.status(204);
+    });
+  },
+
+  putQReport: (req, res) => {
+    models.putQR(req.query).then((data) => {
+      res.status(204);
+    });
+  },
+
+  putAHelpful: (req, res) => {
+    models.putAHelp(req.query).then((data) => {
+      res.status(204);
+    });
+  },
+
+  putAReport: (req, res) => {
+    models.putAR(req.query).then((data) => {
+      res.status(204);
     });
   },
 
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 const controllers = require('./controllers');
 
 router.get('/qa/questions', controllers.getAll);
+router.get('/qa/questions/:question_id/answers', controllers.getAnswers);
+router.post('/qa/questions', controllers.postQuestion);
+router.post('/qa/questions/:question_id/answers', controllers.postAnswer);
+router.put('/qa/questions/:question_id/helpful', controllers.putQHelpful);
+router.put('/qa/questions/:question_id/report', controllers.putQReport);
+router.put('/qa/answers/:answer_id/helpful', controllers.putAHelpful);
+router.put('/qa/answers/:answer_id/report', controllers.putAReport);
 
 
 
